9bb11851cbe374d04566a915368b6dad1ac882fc
Brandon Davis <btdavis3378@eagle.fgcu.edu>
2020-03-04 23:22:36 -0500

    Update main.py

--- DATCAR/main.py
+++ DATCAR/main.py
@@ -132,7 +132,7 @@ def findTempHumi():
    
 def saveToCloud():
    oc = owncloud.Client('http://192.168.1.66/owncloud')
-   oc.login('btdavis3378', 'rwv3ygeb')
+   oc.login('username', 'password')
    oc.put_file('testdir/remotefile.txt', 'saved data.txt')
    link_info = oc.share_file_with_link('testdir/remotefile.txt')
    print ("Here is your link: " + link_info.get_link())
